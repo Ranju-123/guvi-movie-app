24e7d7ad3c749324eaf8917f8add29f59974b6d9
Ranju-123 <ranjithacvj@gmail.com>
2022-07-01 11:49:13 +0530

    message list

--- src/App.js
+++ src/App.js
@@ -2,24 +2,63 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const names=["Nihasini","Mayuri","Nuthan","Kathyahini"];
+  const students=[{
+    name:"NirviVijay",
+     pic:"https://www.bing.com/th?id=OIP.qbkfsNc_YEfMtW4UsBAkngHaIJ&w=150&h=165&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+
+  },
+  {
+     name:"Manasvisumanth",
+      pic:"https://www.bing.com/th?id=OIP.BYlUhGXWlMNsV1FFUisMFAHaH7&w=150&h=161&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+  },
+  {
+name:"Hema",
+ pic:"https://th.bing.com/th/id/OIP.5e35Ud8J0pkczmoEL7Bd1wHaFj?w=259&h=194&c=7&r=0&o=5&pid=1.7"
+  }
+];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*
+      <Message name="NirviVijay" pic="https://www.bing.com/th?id=OIP.qbkfsNc_YEfMtW4UsBAkngHaIJ&w=150&h=165&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" />
+      <Message name="Manasvisumanth" pic="https://www.bing.com/th?id=OIP.BYlUhGXWlMNsV1FFUisMFAHaH7&w=150&h=161&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"/>
+  <Message name="Family" pic="https://th.bing.com/th/id/OIP.5e35Ud8J0pkczmoEL7Bd1wHaFj?w=259&h=194&c=7&r=0&o=5&pid=1.7"/> */}
+{/*<Welcome name="Nihasini"/>
+<Welcome name="Mayuri"/>
+<Welcome name="Nuthan"/>*/}
+{/*component +Loop*/}
+{students.map(
+  (student)=>(<Message name={student.name} pic={student.pic} />)
+  )}
+
+{/* {names.map((nm)=> (
+<Welcome name={nm} />
+  ))} */}
     </div>
   );
 }
 
 export default App;
+function Welcome(props) {
+  
+  return(
+    <div>
+   
+  <h1>Hello,{props.name} </h1>
+  </div>
+  );
+ 
+}
+
+function Message(props) {
+  //const name=props.name;
+  
+  return(
+    <div>
+    <img className="profile-pic" src={props.pic} alt={props.name}/> 
+  <h1>Hello,{props.name}</h1>
+  </div>
+  );
+ 
+}
